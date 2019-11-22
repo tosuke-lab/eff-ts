8a098ae29460247e884d88435f729910f10d58d5
Tosuke <tasukeprg@gmail.com>
2019-11-23 00:44:20 +0900

    refactor: rewrite State with ModifyEffect

--- src/state.ts
+++ src/state.ts
@@ -4,53 +4,39 @@ import { Leaf } from "./arr";
 
 declare const tag: unique symbol;
 
-interface GetEffect<S, Tag> extends Effect<S> {
+interface ModifyEffect<S, Tag> extends Effect<S> {
   [tag]: Tag;
-  readonly name: "GetEffect";
+  readonly name: "ModifyEffect";
+  readonly f: (state: S) => S;
 }
 
-interface SetEffect<S, Tag> extends Effect<void> {
-  [tag]: Tag;
-  readonly name: "SetEffect";
-  readonly value: S;
-}
-
-type StateEffect<S, Tag> = GetEffect<S, Tag> | SetEffect<S, Tag>;
+type StateEffect<S, Tag> = ModifyEffect<S, Tag>;
 
 type State<S, Tag> = {
-  GetEffect: new () => GetEffect<S, Tag>;
-  SetEffect: new (value: S) => SetEffect<S, Tag>;
+  ModifyEffect: new (f: (state: S) => S) => ModifyEffect<S, Tag>;
   handle: (
     initial: S
   ) => <IE extends AnyEffect, A>(
     fx: Eff<IE, A>
   ) => Eff<Exclude<IE, StateEffect<S, Tag>>, [A, S]>;
-  get: () => Eff<GetEffect<S, Tag>, S>;
-  set: (value: S) => Eff<SetEffect<S, Tag>, void>;
-  modify: (f: (state: S) => S) => Eff<StateEffect<S, Tag>, void>;
+  modify: (f: (state: S) => S) => Eff<StateEffect<S, Tag>, S>;
+  get: () => Eff<StateEffect<S, Tag>, S>;
+  put: (value: S) => Eff<StateEffect<S, Tag>, void>;
 };
 
 export const createState = <S, Tag = "">(): State<S, Tag> => {
-  const GetEffect = class GetEffect extends Effect<S> {
+  const ModifyEffect = class ModifyEffect extends Effect<S> {
     [tag]!: Tag;
-    readonly name = "GetEffect" as const;
-  };
+    readonly name = "ModifyEffect" as const;
 
-  const SetEffect = class SetEffect extends Effect<void> {
-    [tag]!: Tag;
-    readonly name = "SetEffect" as const;
-
-    constructor(readonly value: S) {
+    constructor(readonly f: (state: S) => S) {
       super();
     }
   };
 
-  const isGetEff = <A>(
+  const isStateEff = <A>(
     fx: Impure<AnyEffect, A>
-  ): fx is Impure<GetEffect<S, Tag>, A> => fx.effect instanceof GetEffect;
-  const isSetEff = <A>(
-    fx: Impure<AnyEffect, A>
-  ): fx is Impure<SetEffect<S, Tag>, A> => fx.effect instanceof SetEffect;
+  ): fx is Impure<ModifyEffect<S, Tag>, A> => fx.effect instanceof ModifyEffect;
   const handle = (initial: S) => <IE extends AnyEffect, A>(
     fx: Eff<IE, A>
   ): Eff<Exclude<IE, StateEffect<S, Tag>>, [A, S]> => {
@@ -59,10 +45,9 @@ export const createState = <S, Tag = "">(): State<S, Tag> => {
       state: S
     ): Eff<Exclude<IE, StateEffect<S, Tag>>, [A, S]> {
       if (f instanceof Pure) return (f as Eff<never, A>).map(x => [x, state]);
-      if (isGetEff(f)) {
-        return internal(f.k.apply(state), state);
-      } else if (isSetEff(f)) {
-        return internal(f.k.apply(undefined), f.effect.value);
+      if (isStateEff(f)) {
+        const nextState = f.effect.f(state);
+        return internal(f.k.apply(nextState), nextState);
       } else {
         return new Impure<any, [A, S]>(
           f.effect,
@@ -73,19 +58,16 @@ export const createState = <S, Tag = "">(): State<S, Tag> => {
     return internal(fx, initial);
   };
 
-  const get = Eff.liftF(GetEffect);
-  const set = Eff.liftF(SetEffect);
-  const modify = (f: (state: S) => S) =>
-    get()
-      .map(f)
-      .chain(set);
+  const modify = Eff.liftF(ModifyEffect);
+
+  const get = () => modify(s => s);
+  const put = (newState: S) => modify(_ => newState).map(() => {});
 
   return {
-    GetEffect,
-    SetEffect,
+    ModifyEffect,
     handle,
+    modify,
     get,
-    set,
-    modify
+    put
   };
 };

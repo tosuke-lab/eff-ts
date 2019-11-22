5717d863e510db0075f4ee3c1b209b40a0af4962
Tosuke <tasukeprg@gmail.com>
2019-11-23 01:09:41 +0900

    [BREAKING CHANGE] feat: monoidic Writer

--- src/writer.ts
+++ src/writer.ts
@@ -2,36 +2,6 @@ import { Effect, AnyEffect } from "./effect";
 import { createHandler } from "./handler";
 import { Eff } from "./eff";
 
-export type List<A> = Zero | Cons<A>;
-
-export class Zero {
-  toArray(): never[] {
-    return [];
-  }
-
-  cons<A>(value: A): List<A> {
-    return new Cons<A>(value, this);
-  }
-}
-
-export class Cons<A> {
-  constructor(readonly value: A, readonly next: List<A>) {}
-
-  toArray(): A[] {
-    const array: A[] = [];
-    let list: List<A> = this;
-    while (true) {
-      if (list instanceof Zero) return array;
-      array.push(list.value);
-      list = list.next;
-    }
-  }
-
-  cons(value: A): List<A> {
-    return new Cons(value, this);
-  }
-}
-
 declare const tag: unique symbol;
 
 interface TellEffect<W, Tag> extends Effect<void> {
@@ -40,11 +10,13 @@ interface TellEffect<W, Tag> extends Effect<void> {
   readonly value: W;
 }
 
+type WriterHandler<W, Tag, M> = <IE extends AnyEffect, A>(
+  fx: Eff<IE, A>
+) => Eff<Exclude<IE, TellEffect<W, Tag>>, readonly [A, M]>;
+
 type Writer<W, Tag> = {
   TellEffect: new (value: W) => TellEffect<W, Tag>;
-  handle: <IE extends AnyEffect, A>(
-    fx: Eff<IE, A>
-  ) => Eff<Exclude<IE, TellEffect<W, Tag>>, [A, List<W>]>;
+  handle: <M>(zero: M, fold: (pre: M, cur: W) => M) => WriterHandler<W, Tag, M>;
   tell: (value: W) => Eff<TellEffect<W, Tag>, void>;
 };
 
@@ -58,18 +30,19 @@ export const createWriter = <W, Tag = "">(): Writer<W, Tag> => {
     }
   };
 
-  type Writer<A> = [A, List<W>];
-  const handle = createHandler(
-    <A>(x: A) => Eff.pure<Writer<A>>([x, new Zero()]),
-    ({ construct, match }) =>
-      construct(
-        match(TellEffect, (e, k) => {
-          return k().map(
-            ([x, list]) => [x, list.cons(e.value)] as Writer<typeof x>
-          );
-        })
-      )
-  );
+  const handle = <M>(
+    zero: M,
+    fold: (pre: M, cur: W) => M
+  ): WriterHandler<W, Tag, M> =>
+    createHandler(
+      <A>(x: A) => Eff.pure([x, zero] as const),
+      ({ construct, match }) =>
+        construct(
+          match(TellEffect, (e, k) => {
+            return k().map(([x, m]) => [x, fold(m, e.value)] as const);
+          })
+        )
+    );
 
   const tell = Eff.liftF(TellEffect);
 
